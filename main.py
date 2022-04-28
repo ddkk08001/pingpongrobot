e52af928cd35495114ddac66a2a6f02fc2784dee
ddkk08 <daniel.sh08.kim@gmail.com>
2022-04-28 19:05:44 +0200

    📋Main File

--- main.py
+++ main.py
@@ -13,13 +13,25 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
 # Create your objects here.
+
+#Initialize
 ev3 = EV3Brick()
 
-POSSIBLE_COLORS = (Color)
+#possable colors inputs
+POSSIBLE_COLORS = (Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.WHITE)
 
+#Initialize motor
 base_motor = Motor(Port.A, Direction.COUNTERCLOCKWISE, [8, 40])
-base_switch = Sensor(Port.S1)
+second_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE, [8, 40])
+
+#Initialize sensor
+base_switch = ColorSensor(Port.S1)
 
 # Write your program here.
-while base_switch(Color.Reflect)
-    ###여기서부터 작업
+
+#Main loop
+while True:
+    color_list = []
+
+    while len(color_list) < 8:
+        base_motor.run(-100)
